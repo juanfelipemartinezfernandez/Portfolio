6fa71c2b3ac48e5e5f38c7ad61c9159c73d6a83c
juanfelipemartinezfernandez <juanfelipe.martinez.fernandez@gmail.com>
2023-08-16 21:17:21 -0500

    Fix  tags, and change home style

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function goToIt(){
   const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
   scrollTo({
-    top:vh+50,
-    left:110,
+    top:vh,
     behavior: "smooth"
     });
   
